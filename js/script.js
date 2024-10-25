8b75def45b16e3d14ee8c68c8e998ada7c035b32
tushar312341 <tb3123417@gmail.com>
2024-10-25 20:08:45 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// JavaScript to add smooth scroll behavior to navigation links
+document.querySelectorAll('nav ul li a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        const targetId = this.getAttribute('href').substring(1);
+        const targetSection = document.getElementById(targetId);
+        window.scrollTo({
+            top: targetSection.offsetTop - 50,
+            behavior: 'smooth'
+        });
+    });
+});
